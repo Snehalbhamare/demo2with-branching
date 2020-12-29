94c34f1a95dcba423dd6350cae475a04eda3fd8d
Snehalbhamare <snehalbhamare9@gmail.com>
2020-12-29 13:42:48 +0530

    adding new coded code

--- qa.pages/src/test/java/qa/pages/pages.java
+++ qa.pages/src/test/java/qa/pages/pages.java
@@ -7,5 +7,10 @@ public static void main(String args[])
 {
 System.out.println("hello...hello...helloo");	
 System.out.println("hello...hello...helloo");	
+
+
+System.out.println("hello...hello...helloo");
+System.out.println("hello...hello...helloo");
+System.out.println("hello...hello...helloo");
 }
 }
