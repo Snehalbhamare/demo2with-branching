52181c8de994defa955946a76b9df2bc9f803b22
Snehalbhamare <snehalbhamare9@gmail.com>
2020-12-29 13:29:19 +0530

    trying with branches

---
+++ qa.pages/src/test/java/qa/pages/pages.java
@@ -0,0 +1,11 @@
+package qa.pages;
+
+public class pages 
+
+{
+public static void main(String args[])
+{
+System.out.println("hello...hello...helloo");	
+System.out.println("hello...hello...helloo");	
+}
+}
